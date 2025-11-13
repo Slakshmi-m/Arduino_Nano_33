cef3344ca72a28d69592ec4b367e454a83202ce5
Sreelakshmi M Rajeev <58187592+Slakshmi-m@users.noreply.github.com>
2025-11-13 12:07:36 +0000

    Servo motor code

---
+++ nano_Servo.ino
@@ -0,0 +1,22 @@
+#include<Servo.h>
+
+Servo myServo;
+int pos = 0;
+
+void setup(){
+    Serial.begin(9600);
+    myServo.attach(7);
+    myServo.write(0)
+}
+
+void loop(){
+    for (pos = 0; pos <= 180; pos += 2){
+        myServo.write(pos);
+        delay(15);
+    }
+
+    for(pos = 180; pos >= 0, pos -= 2){
+        myServo.write(pos);
+        delay(15);
+    }
+}
